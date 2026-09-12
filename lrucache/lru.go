bbec03343803cfc3229c91c62f2d0187c7af6bfd
Алтунина Екатерина <muchashamedofyou@gmail.com>
2026-09-13 01:48:44 +0300

    part task 2

--- lrucache/lru.go
+++ lrucache/lru.go
@@ -2,6 +2,71 @@
 
 package lrucache
 
+import "container/list"
+
+type item struct {
+	key   int
+	value int
+}
+
+type Lrucache struct {
+	capacity int
+	time     *list.List
+	storage  map[int]*list.Element
+}
+
+// Clear implements [Cache].
+func (l *Lrucache) Clear() {
+	l.time.Init()
+	for k := range l.storage {
+		delete(l.storage, k)
+	}
+}
+
+// Get implements [Cache].
+func (l *Lrucache) Get(key int) (int, bool) {
+	val, ok := l.storage[key]
+	if !ok {
+		return 0, false
+	}
+	l.time.MoveToFront(val)
+	return val.Value.(*item).value, true
+}
+
+// Range implements [Cache].
+func (l *Lrucache) Range(f func(key int, value int) bool) {
+	for i := l.time.Back(); i != nil; i = i.Prev() {
+		// l.time.MoveToFront(i)
+		it := i.Value.(*item)
+		if !f(it.key, it.value) {
+			return
+		}
+	}
+}
+
+// Set implements [Cache].
+func (l *Lrucache) Set(key int, value int) {
+	if l.capacity == 0 {
+		return
+	}
+	if el, ok := l.storage[key]; ok {
+		el.Value.(*item).value = value
+		l.time.MoveToFront(el)
+		return
+	}
+	if len(l.storage) >= l.capacity {
+		back := l.time.Back()
+		l.time.Remove(back)
+		delete(l.storage, back.Value.(*item).key)
+	}
+	node := l.time.PushFront(&item{key: key, value: value})
+	l.storage[key] = node
+}
+
 func New(cap int) Cache {
-	panic("implement me")
+	return &Lrucache{
+		capacity: cap,
+		storage:  make(map[int]*list.Element),
+		time:     list.New(),
+	}
 }
--- otp/cipher.go
+++ otp/cipher.go
@@ -6,10 +6,71 @@ import (
 	"io"
 )
 
+type cipherReader struct {
+	r    io.Reader
+	prng io.Reader
+	buf  []byte
+}
+
+func (cr *cipherReader) Read(p []byte) (n int, err error) {
+	n, err = cr.r.Read(p)
+	if n == 0 {
+		return 0, err
+	}
+
+	if len(cr.buf) < n {
+		cr.buf = make([]byte, n)
+	}
+	key := cr.buf[:n]
+
+	if _, err := io.ReadFull(cr.prng, key); err != nil {
+		return 0, err
+	}
+
+	for i := 0; i < n; i++ {
+		p[i] ^= key[i]
+	}
+
+	return n, err
+}
+
 func NewReader(r io.Reader, prng io.Reader) io.Reader {
-	panic("implement me")
+	return &cipherReader{
+		r:    r,
+		prng: prng,
+		buf:  make([]byte, 0),
+	}
+}
+
+type cipherWriter struct {
+	w    io.Writer
+	prng io.Reader
+	buf  []byte
+}
+
+func (cw *cipherWriter) Write(p []byte) (n int, err error) {
+	if len(cw.buf) != len(p) {
+		cw.buf = make([]byte, len(p))
+	}
+	key := cw.buf[:len(p)]
+
+	if _, err := io.ReadFull(cw.prng, key); err != nil {
+		return 0, err
+	}
+
+	enc := make([]byte, len(p))
+	for i := range p {
+		enc[i] = p[i] ^ key[i]
+	}
+
+	n, err = cw.w.Write(enc)
+	return n, err
 }
 
 func NewWriter(w io.Writer, prng io.Reader) io.Writer {
-	panic("implement me")
+	return &cipherWriter{
+		w:    w,
+		prng: prng,
+		buf:  make([]byte, 0),
+	}
 }
