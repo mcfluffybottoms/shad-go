ab7c308e44696b3d0aeed9a18b24d8074909cff8
Алтунина Екатерина <muchashamedofyou@gmail.com>
2026-09-14 21:27:47 +0300

    part tast 2 done

--- externalsort/sort.go
+++ externalsort/sort.go
@@ -3,21 +3,166 @@
 package externalsort
 
 import (
+	"bufio"
+	"container/heap"
 	"io"
+	"os"
+	"sort"
 )
 
+type item struct {
+	line     string
+	readerId int
+}
+
+type lineHeap []item
+
+func (h lineHeap) Len() int {
+	return len(h)
+}
+
+func (h lineHeap) Less(i, j int) bool {
+	return h[i].line < h[j].line
+}
+
+func (h lineHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *lineHeap) Push(x any) {
+	*h = append(*h, x.(item))
+}
+
+func (h *lineHeap) Pop() any {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	*h = old[:n-1]
+	return item
+}
+
+type Reader struct {
+	sc *bufio.Reader
+}
+
+func (r *Reader) ReadLine() (string, error) {
+	line, err := r.sc.ReadString('\n')
+	if err == io.EOF {
+		if len(line) > 0 {
+			return line, nil
+		} else {
+			return "", io.EOF
+		}
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	return line[:len(line)-1], nil
+}
+
 func NewReader(r io.Reader) LineReader {
-	panic("implement me")
+	return &Reader{sc: bufio.NewReader(r)}
+}
+
+type Writer struct {
+	w io.Writer
+}
+
+func (w *Writer) Write(l string) error {
+	_, err := io.WriteString(w.w, l+"\n")
+	return err
 }
 
 func NewWriter(w io.Writer) LineWriter {
-	panic("implement me")
+	return &Writer{w: w}
+}
+
+func readLineToHeap(h *lineHeap, r LineReader, readerId int) error {
+	line, err := r.ReadLine()
+	if err == io.EOF {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	heap.Push(h, item{readerId: readerId, line: line})
+	return nil
 }
 
 func Merge(w LineWriter, readers ...LineReader) error {
-	panic("implement me")
+	h := &lineHeap{}
+	heap.Init(h)
+
+	for i, r := range readers {
+		if err := readLineToHeap(h, r, i); err != nil {
+			return err
+		}
+	}
+
+	for h.Len() > 0 {
+		it := heap.Pop(h).(item)
+		if err := w.Write(it.line); err != nil {
+			return err
+		}
+		if err := readLineToHeap(h, readers[it.readerId], it.readerId); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func Sort(w io.Writer, in ...string) error {
-	panic("implement me")
+	readers := make([]LineReader, len(in))
+	for i, p := range in {
+		// read and sort
+		file1, err := os.Open(p)
+		if err != nil {
+			return err
+		}
+		lr := NewReader(file1)
+		lines := make([]string, 0)
+		for {
+			line, err := lr.ReadLine()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				file1.Close()
+				return err
+			}
+			lines = append(lines, line)
+		}
+		if err := file1.Close(); err != nil {
+			return err
+		}
+		sort.Strings(lines)
+
+		// write to file
+		file2, err := os.Create(p)
+		if err != nil {
+			return err
+		}
+
+		lw := NewWriter(file2)
+		for _, line := range lines {
+			if err := lw.Write(line); err != nil {
+				file2.Close()
+				return err
+			}
+		}
+		if err := file2.Close(); err != nil {
+			return err
+		}
+		// add reader to readers
+		file3, err := os.Open(p)
+		if err != nil {
+			return err
+		}
+		defer file3.Close()
+		readers[i] = NewReader(file3)
+	}
+	return Merge(NewWriter(w), readers...)
 }
--- retryupdate/update.go
+++ retryupdate/update.go
@@ -2,8 +2,81 @@
 
 package retryupdate
 
-import "gitlab.com/slon/shad-go/retryupdate/kvapi"
+import (
+	"errors"
+
+	"github.com/gofrs/uuid"
+	"gitlab.com/slon/shad-go/retryupdate/kvapi"
+)
 
 func UpdateValue(c kvapi.Client, key string, updateFn func(oldValue *string) (newValue string, err error)) error {
-	panic("implement me")
+	var authErr *kvapi.AuthError
+	var apiErr *kvapi.APIError
+	var conflictErr *kvapi.ConflictError
+
+	runGet := true
+	var err error
+	var getRequest *kvapi.GetRequest
+	var getResponse *kvapi.GetResponse
+
+	var responseValue *string
+	var responseUUID uuid.UUID
+	for {
+		if runGet {
+			getRequest = &kvapi.GetRequest{Key: key}
+			getResponse, err = c.Get(getRequest)
+			runGet = false
+
+			switch {
+			case err == nil:
+				responseValue = &getResponse.Value
+				responseUUID = getResponse.Version
+			case errors.As(err, &authErr):
+				return err
+			case errors.As(err, &apiErr):
+				unwrapped := apiErr.Unwrap()
+				if unwrapped == kvapi.ErrKeyNotFound {
+					responseValue = nil
+					responseUUID = uuid.Nil
+				} else {
+					runGet = true
+					continue
+				}
+			default:
+				runGet = true
+				continue
+			}
+		}
+
+		newValue, err := updateFn(responseValue)
+		if err != nil {
+			return err
+		}
+
+		setRequest := kvapi.SetRequest{Key: key, Value: newValue, OldVersion: responseUUID}
+		_, err = c.Set(&setRequest)
+
+		if err == nil {
+			return nil
+		}
+
+		switch {
+		case errors.As(err, &conflictErr):
+			if conflictErr.ExpectedVersion == uuid.Nil {
+				return nil
+			}
+			runGet = true
+		case errors.As(err, &apiErr):
+			unwrapped := apiErr.Unwrap()
+			switch {
+			case unwrapped == kvapi.ErrKeyNotFound:
+				responseValue = nil
+				responseUUID = uuid.Nil
+			case errors.As(unwrapped, &authErr):
+				return err
+			}
+		default:
+			return err
+		}
+	}
 }
