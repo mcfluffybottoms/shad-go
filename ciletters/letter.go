020dc254b8577c50bc209610f9a04c0bfc26b7cb
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2026-09-16 02:53:18 +0300

    task 2 done

--- ciletters/letter.go
+++ ciletters/letter.go
@@ -2,6 +2,53 @@
 
 package ciletters
 
+import (
+	"bytes"
+	_ "embed"
+	"strings"
+	"text/template"
+)
+
+//go:embed letter.tmpl
+var letterTemplate string
+
 func MakeLetter(n *Notification) (string, error) {
-	return "", nil
+	tmpl, err := template.New("ci").Funcs(
+		template.FuncMap{
+			"isPipelineOK": func(status PipelineStatus) bool {
+				return status == PipelineStatusOK
+			},
+			"trimHash": func(hash string) string {
+				return hash[:8]
+			},
+			"last10lines": func(log string) []string {
+				lines := strings.Split(log, "\n")
+				var start int
+				if len(lines) > 10 {
+					start = len(lines) - 10
+				} else {
+					start = 0
+				}
+				return lines[start:]
+			},
+		}).Parse(letterTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	data := struct {
+		Notification
+		PipelineStatusOK PipelineStatus
+	}{
+		Notification:     *n,
+		PipelineStatusOK: PipelineStatusOK,
+	}
+
+	var out bytes.Buffer
+	err = tmpl.Execute(&out, data)
+	if err != nil {
+		return "", err
+	}
+
+	return out.String(), nil
 }
--- yamlembed/types.go
+++ yamlembed/types.go
@@ -1,19 +1,61 @@
 package yamlembed
 
+import (
+	"strings"
+)
+
 type Foo struct {
-	A string
+	A string `yaml:"aa"`
 	p int64
 }
 
 type Bar struct {
-	I      int64
-	B      string
-	UpperB string
-	OI     []string
-	F      []any
+	I      int64    `yaml:"-"`
+	B      string   `yaml:"b"`
+	UpperB string   `yaml:"-"`
+	OI     []string `yaml:"-"`
+	F      []any    `yaml:"f,flow"`
+}
+
+func (b *Bar) UnmarshalYAML(unmarshal func(any) error) error {
+	var v struct {
+		B  string   `yaml:"b"`
+		OI []string `yaml:"oi"`
+		F  []any    `yaml:"f"`
+	}
+	if err := unmarshal(&v); err != nil {
+		return err
+	}
+	b.B = v.B
+	b.UpperB = strings.ToUpper(v.B)
+	b.OI = v.OI
+	b.F = v.F
+	return nil
 }
 
 type Baz struct {
 	Foo `yaml:",inline"`
 	Bar `yaml:",inline"`
+}
+
+func (b *Baz) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var v struct {
+		A  string   `yaml:"aa"`
+		B  string   `yaml:"b"`
+		OI []string `yaml:"oi"`
+		F  []any    `yaml:"f"`
+	}
+
+	if err := unmarshal(&v); err != nil {
+		return err
+	}
+
+	b.Foo.A = v.A
+
+	b.Bar.B = v.B
+	b.Bar.UpperB = strings.ToUpper(v.B)
+	b.Bar.OI = v.OI
+	b.Bar.F = v.F
+
+	return nil
 }
